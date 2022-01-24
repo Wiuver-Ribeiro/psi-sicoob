647e63cd733501de3dffcb377740e97d4bc936a0
Wiuver Afonso Ribeiro <52933241+Wiuver-Ribeiro@users.noreply.github.com>
2022-01-24 12:42:20 -0300

    Estrutura do Projeto inicial

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -1,1 +1,21 @@
-<h1>Header</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" href="<?php echo $base.'/assets/icons/scs.ico';?>" type="image/x-icon" />            
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo $base.'/assets/css/reset.css'?>" />
+  <link rel="stylesheet" href="<?php echo $base.'/assets/css/header.css'?>" />
+  <title>PSI - Sicoob Centro-sul</title>
+</head>
+<body>
+  <header>
+    <ul>
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">Sobre</a></li>
+      <li><a href="#services">Serviços</a></li>
+      <li><a href="#psi">Psicologos</a></li>
+    </ul>
+
+  </header>
