be724869154d4f34dcbd667d365396565303832a
wiuver.ribeiro <wiuver.ribeiro@gmail.com>
2022-01-24 21:57:21 -0300

    Atualizações do main em andamento

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,19 +1,23 @@
-<!-- Inclusão do header -->
+<!--Inclusão do header -->
 <?php $render('header'); ?>
-<link rel="stylesheet" href="<?php echo $base.'/assets/css/components/main.css'?>">
+<link rel="stylesheet" href="<?php echo $base . '/assets/css/components/main.css' ?>">
+
 <main class="container-main">
-  <section class="container-principal">
+  <section class="container">
     <div class="informacoes-principais">
-      <span>Estamos aqui pra você</span>
-    <h1 style="font-size:2.5rem;">Os melhores psicólogos</h1>
-    <p style="font-size:1.2rem;">Aqui na PSI, você encontra os melhores psicólogos da região, com atendimento diferenciado e um  
-      agendamento muito fácil.
-    </p>
+      <span style="font-size: 1.2rem;">Estamos aqui pra você</span>
+      <h1 class="title-principal">Os melhores psicólogos</h1>
+      <p style="font-size:1.2rem;text-align: center;">Aqui na PSI, você encontra os melhores psicólogos da região, com atendimento diferenciado e um
+        agendamento muito fácil.
+      </p>
+      <a href="#">AGENDE SUA CONSULTA</a>
     </div>
-    <img src="<?php echo $base.'/assets/pictures/doctors1.png'; ?>" alt="Wallpaper Doctors" width="500px" height="500px">
+
+    <div class="doctor">
+      <img src="<?php echo $base . '/assets/pictures/doctors1.png'; ?>" alt="Wallpaper Doctors" width="600px" height="600px">
+    </div>
+  </section>
+  <section id="sobre" class="container">
+   <img src="<?php echo $base.'/assets/pictures/calendar.png'?>" alt="" width="700px" height="700px">
   </section>
 </main>
-
-
-<!-- Inclusão do Footer -->
-<?php $render('footer'); ?>
