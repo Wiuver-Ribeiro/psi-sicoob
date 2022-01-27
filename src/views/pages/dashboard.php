099ba75254d327d44498a8acb45634413505f52f
Wiuver Afonso Ribeiro <52933241+Wiuver-Ribeiro@users.noreply.github.com>
2022-01-27 13:39:02 -0300

    aside bar finalizada

--- src/views/pages/dashboard.php
+++ src/views/pages/dashboard.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="<?php echo $base.'/assets/css/reset.css'; ?>" >
-  <link rel="stylesheet" href="<?php echo $base.'/assets/css/components/sidebar.css'; ?>" >
-  <link rel="stylesheet" href="<?php echo $base.'/assets/css/components/navbar.css'; ?>" >
-  
+  <link rel="stylesheet" href="<?php echo $base . '/assets/css/reset.css'; ?>">
+  <link rel="stylesheet" href="<?php echo $base . '/assets/css/components/sidebar.css'; ?>">
+  <link rel="stylesheet" href="<?php echo $base . '/assets/css/components/navbar.css'; ?>">
+
   <title>PSI | Dashboard</title>
 </head>
+
 <body>
-    <?php $render('sidebar'); ?>
-    <?php $render('navbar'); ?>
+  <?php $render('sidebar'); ?>
+  <?php $render('navbar'); ?>
+
+  <main class="container">
+    <h1>Hello World!</h1>
+  </main>
+
 </body>
+
 </html>
 <script src="https://kit.fontawesome.com/dba7af9f9b.js" crossorigin="anonymous"></script>
--- src/views/partials/sidebar.php
+++ src/views/partials/sidebar.php
@@ -3,4 +3,17 @@
     <img src="<?php echo $base.'/assets/icons/icon_app.png';?>" alt="Logo">
     PSI - Sicoob Centro-sul
   </div>
+
+  <div class="box-sidebar">
+    <span>MENU</span>
+    <ul>
+      <li><a href="#"><i class="fas fa-home" style="margin-right: 1rem"></i>Dashboard</a></li>
+      <li><i class="fas fa-calendar" style="color:#01A9AC; margin-right:1rem;"></i>Agendamentos</li>
+      <li><i class="fas fa-user-friends" style="color:#9595DF; margin-right:1rem;"></i>Médicos</li>
+      <li><i class="fas fa-user-alt" style="color:#8DC9E8; margin-right:1rem;"></i>Pacientes</li>
+      <li><i class="fas fa-lock" style="color:#F4C22B; margin-right:1rem;"></i>Administradores</li>
+      <li><i class="fas fa-cog" style="color:#4DC6FA; margin-right:1rem;" style="color:#1CA9F5; margin-right:1rem;"></i>Configuração</li>
+      <li><i class="fas fa-power-off" style="color:#FF6D8F; margin-right:1rem;"></i>Logout</li>
+    </ul>
+  </div>
 </aside>
