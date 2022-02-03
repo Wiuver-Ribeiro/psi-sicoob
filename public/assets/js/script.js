3b17c2ad62ddaab60b3a25bbc3cf812dca4787e5
Wiuver Afonso Ribeiro <52933241+Wiuver-Ribeiro@users.noreply.github.com>
2022-02-03 13:58:52 -0300

    Atualizações em andamento

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -20,20 +20,67 @@ document.addEventListener('DOMContentLoaded', function() {
     {
       id: 2,
       title: 'Pagar Fatura',
-      start: '2022-02-12 09:00',
-      end: '2022-02-12 12:00'
+      start: '2022-02-12 14:00',
+      end: '2022-02-12 12:00',
+      color: '#8B0000'
     },
    ],
 
     //Fim
     eventClick: function(info) {
-
+      info.jsEvent.preventDefault();
       $('#visualizar #id').text(info.event.id);
       $('#visualizar #title').text(info.event.title);
+      $('#visualizar #title').text(info.event.color);
       $('#visualizar #start').text(info.event.start.toLocaleString());
       $('#visualizar #end').text(info.event.end.toLocaleString());
       $('#visualizar').modal('show');
+    },
+        select: function(info) {
+      // alert('Início do Evento: '+ info.start.toLocaleString());
+      $('#cadastrar #title').val(info.title);
+      $('#cadastrar #title').val(info.color);
+      $('#cadastrar #start').val(info.start.toLocaleString());
+      $('#cadastrar #end').val(info.end.toLocaleString());
+      $('#cadastrar').modal('show');
+      
+      
     }
   }, );
   calendar.render();
 });
+
+
+
+//Mascara para o campo data e hora
+function DataHora(evento, objeto) {
+  var keypress = (window.event) ? event.keyCode : evento.which;
+  campo = eval(objeto);
+  if (campo.value == '00/00/0000 00:00:00') {
+      campo.value = "";
+  }
+
+  caracteres = '0123456789';
+  separacao1 = '/';
+  separacao2 = ' ';
+  separacao3 = ':';
+  conjunto1 = 2;
+  conjunto2 = 5;
+  conjunto3 = 10;
+  conjunto4 = 13;
+  conjunto5 = 16;
+  if ((caracteres.search(String.fromCharCode(keypress)) != -1) && campo.value.length < (19)) {
+      if (campo.value.length == conjunto1)
+          campo.value = campo.value + separacao1;
+      else if (campo.value.length == conjunto2)
+          campo.value = campo.value + separacao1;
+      else if (campo.value.length == conjunto3)
+          campo.value = campo.value + separacao2;
+      else if (campo.value.length == conjunto4)
+          campo.value = campo.value + separacao3;
+      else if (campo.value.length == conjunto5)
+          campo.value = campo.value + separacao3;
+  } else {
+      event.returnValue = false;
+  }
+}
--- src/views/pages/admin/appointments.php
+++ src/views/pages/admin/appointments.php
@@ -29,7 +29,7 @@
   <?php $render('navbar'); ?>
   <?php $render('sidebar'); ?>
   <div id='calendar'></div>
-  <!-- Modal -->
+  <!-- Modal Visualizar -->
   <div class="modal fade" id="visualizar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
@@ -41,6 +41,8 @@
           <dl class="row">
             <dt class="col-sm-3">ID do Evento:</dt>
             <dd class="col-sm-9" id="id"></dd>
+            <dt class="col-sm-3">Cor do Evento:</dt>
+            <dd class="col-sm-9" id="color"></dd>
             <dt class="col-sm-3">Título do Evento:</dt>
             <dd class="col-sm-9" id="title"></dd>
             <dt class="col-sm-3">Início do Evento:</dt>
@@ -49,10 +51,72 @@
             <dd class="col-sm-9" id="end"></dd>
           </dl>
         </div>
-
       </div>
     </div>
   </div>
+  <!--VISUALIZAR-->
+
+
+  <!-- Modal Cadastrar -->
+  <div class="modal fade" id="cadastrar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Agendar Consulta</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="row mb-3">
+              <label for="title" class="col-sm-2 col-form-label">Título do Evento:</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="title" name="title">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="title" class="col-sm-2 col-form-label">Cor do Evento:</label>
+              <div class="col-sm-10">
+                <select name="color" class="form-control" id="color">
+                  <option>Selecione</option>
+                  <option style="color:#FFD700;" value="#FFD700">Amarelo</option>
+                  <option style="color:#0071c5;" value="#0071c5">Azul Turquesa</option>
+                  <option style="color:#FF4500;" value="#FF4500">Laranja</option>
+                  <option style="color:#8B4513;" value="#8B4513">Marrom</option>
+                  <option style="color:#1C1C1C;" value="#1C1C1C">Preto</option>
+                  <option style="color:#436EEE;" value="#436EEE">Royal Blue</option>
+                  <option style="color:#A020F0;" value="#A020F0">Roxo</option>
+                  <option style="color:#40E0D0;" value="#40E0D0">Turquesa</option>
+                  <option style="color:#228B22;" value="#228B22">Verde</option>
+                  <option style="color:#8B0000;" value="#8B0000">Vermelho</option>
+                </select>
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="start" class="col-sm-2 col-form-label">Início do Evento:</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="start" name="start" onkeypress="DataHora(event, this)">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="end" class="col-sm-2 col-form-label">Fim do Evento:</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="end" name="end" onkeypress="DataHora(event, this)">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-sm-10">
+                <button type="submit" class="btn btn-success">Agendar</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--VISUALIZAR-->
+
+
+
   <script src="https://kit.fontawesome.com/dba7af9f9b.js" crossorigin="anonymous"></script>
 
 </body>
