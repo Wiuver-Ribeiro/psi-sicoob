3032a4309f5191e7dee63f4a3e2f50f1ce3f5319
Wiuver Afonso Ribeiro <52933241+Wiuver-Ribeiro@users.noreply.github.com>
2022-03-02 16:54:55 -0300

    Atualizações em andamento

--- src/views/pages/admin/eventos.php
+++ src/views/pages/admin/eventos.php
@@ -3,27 +3,56 @@
 
 
 
-$sql = $pdo->prepare("SELECT id, title, start, end, color, status FROM consultas");
+// $sql = $pdo->prepare("SELECT id, title, start, end, color, status FROM consultas");
+// $sql->execute();
+
+
+// $eventos = array();
+
+// while($dados  = $sql->fetch(\PDO::FETCH_ASSOC)) {
+//   $id = $dados['id'];
+//   $title = $dados['title'];
+//   $start = $dados['start'];
+//   $end = $dados['end'];
+//   $color = $dados['color'];
+//   $status = $dados['status'];
+
+//   $eventos[] = [
+//     'id' => $id,
+//     'title' => $title,
+//     'start' => $start,
+//     'end' => $end,
+//     'color' => $color,
+//     'status' => $status,
+//   ];
+
+// }
+
+$sql = $pdo->prepare("SELECT idagendamentos, title, id_psi, id_paciente, inicio, fim, color, status FROM agendamentos");
 $sql->execute();
 
 
 $eventos = array();
 
 while($dados  = $sql->fetch(\PDO::FETCH_ASSOC)) {
-  $id = $dados['id'];
+  $id = $dados['idagendamentos'];
   $title = $dados['title'];
-  $start = $dados['start'];
-  $end = $dados['end'];
-  $color = $dados['color'];
+  $psi = $dados['id_psi'];
+  $paciente = $dados['id_paciente'];
+  $start = $dados['inicio'];
+  $end = $dados['fim'];
   $status = $dados['status'];
+  $color = $dados['color'];
 
   $eventos[] = [
     'id' => $id,
     'title' => $title,
+    'psi' => $psi,
+    'paciente' => $paciente,
     'start' => $start,
     'end' => $end,
-    'color' => $color,
     'status' => $status,
+    'color' => $color,
   ];
 
 }
