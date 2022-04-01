d0d1c00cb85768fe1befd36da1784bb754aa2a37
Wiuver Afonso Ribeiro <52933241+Wiuver-Ribeiro@users.noreply.github.com>
2022-04-01 14:31:08 -0300

    Tela dos admins em bootstrap concluída

--- src/views/pages/admin/admins.php
+++ src/views/pages/admin/admins.php
@@ -7,62 +7,79 @@
   <link rel="shortcut icon" href="<?php echo $base . '/assets/icons/scs.ico'; ?>" type="image/x-icon" />
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
   <link rel="stylesheet" href="<?php echo $base . '/assets/css/reset.css'; ?>">
   <link rel="stylesheet" href="<?php echo $base . '/assets/css/components/sidebar.css'; ?>">
   <link rel="stylesheet" href="<?php echo $base . '/assets/css/components/navbar.css'; ?>">
   <link rel="stylesheet" href="<?php echo $base . '/assets/css/components/admins.css'; ?>">
+  <style>
+    .container {
+      height: 100vh;
+      width: calc(100% - 270px);
+      margin-left: 270px;
+      overflow: hidden;
+    }
 
+    img {
+      width: 80px;
+      object-fit: contain !important;
+    }
+    .hover {
+      cursor: pointer !important;
+    }
+  </style>
   <title>PSI | Administradores</title>
 </head>
 
 <body>
-  <?php $render('navbar'); ?>
-  <?php $render('sidebar'); 
+  <?php
+
+  use \src\models\USer;
+
+  $usuario = new User();
+  $info = $usuario->dadosLogado();
+
+  $render('navbar'); ?>
+  <?php $render('sidebar');
   //SESSÕES
-  if(isset($_SESSION['email'])) {
+  if (isset($_SESSION['email'])) {
     echo $_SESSION['email'];
     unset($_SESSION['email']);
     $_SESSION['email'] = '';
   }
 
   ?>
-  <main>
-    <div class="main-container">
-      <section class="default">
-        <h3 style="text-align:left">Administradores</h3>
-        <div class="content-psi">
-          <div class="content-psi-header">
-            <h4 style="font-weight:500">Todos os Administradores</h4>
-            <button>
-              <a href="<?php echo $base . '/admins/create'; ?>" style="color:blue;"><i class="fas fa-user-plus"></i></a>
-            </button>
+
+  <main class="container p-5 bg-dark">
+    <h2 class="text-light mb-4">Administradores:</h2>
+    <div class="container-fluid rounded p-4" style="background: #151419">
+      <div class="column d-flex justify-content-between">
+        <h4 class="text-light">Todos os Administradores:</h4>
+        <?php echo ($info['tipo'] == "admin") ? " <a class='btn btn-primary' href='" . $base . "/admins/create'><i class='fas fa-user-plus'></i></a>" : "" ?>
+      </div>
+      <hr class="text-light">
+
+      <div class="row justify-content-between mb-2 p-3">
+        <?php foreach ($administradores as $administrador) : ?>
+          <div class="hover col-lg-5 col-md-7 cols-m-1 bg-dark mr-3 mb-4 p-3 d-flex justify-content-between align-items-center rounded shadow-sm" onclick="location.href='http://localhost/psi-sicoob/public/admins/edit/<?php echo $administrador['idusuario'];?>'">
+            <img class="img-fluid rounded-circle " src="<?php echo $base . '/assets/icons/' . $administrador['avatar']; ?>" alt="">
+            <div class="row ">
+              <span class="text-light text-center fs-5"><?php echo $administrador['nome']; ?></span>
+              <span class="text-light text-center fs-6"><?php echo $administrador['email']; ?></span>
+
+            </div>
           </div>
-          <div class="grid-doctors">
-            <?php foreach ($administradores as $administrador): ?>
-              <div class="doctor-box" name="id" onclick="location.href='http://localhost/psi-sicoob/public/admins/edit/<?php echo $administrador['idusuario'];?>'">
-                <img src="<?php echo $base . '/assets/icons/'.$administrador['avatar']; ?>" alt="">
-                <div class="doctor-info">
-                  <span><?php echo $administrador['nome'];?></span>
-                  <span><?php echo $administrador['email']; ?></span>
-                </div>
-              </div>
-              <?php endforeach; ?>
-            <!---doctor-box-->
-          </div>
-          <!---grid-doctors-->
-        </div>
-        <!---content-psi--->
-      </section>
-    </div>
-    <!--main-container-->
+        <?php endforeach; ?>
+      </div>
+      <!--row-pai-->
   </main>
 
 
+  <script src="<?php echo $base; ?>/assets/js/script.js"></script>
+  <script src="<?php echo $base . '/assets/js/personalizado.js'; ?>"></script>
 
+  <script src="https://kit.fontawesome.com/dba7af9f9b.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
-<script src="<?php echo $base; ?>/assets/js/script.js"></script>
-<script src="<?php echo $base . '/assets/js/personalizado.js'; ?>"></script>
-
-<script src="https://kit.fontawesome.com/dba7af9f9b.js" crossorigin="anonymous"></script>
